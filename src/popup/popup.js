6afa3b1f5142990f30e90da93e3e01be42d60e5a
Mitchell Kossoris <mkossoris@gmail.com>
2018-09-08 05:26:19 -0500

    Redesigned app initial commit
    Added new icons, removed old ones. Updated manifest with new icons.
    Updated structure and design of extension to be more modern. Added
    lyrics and links for song recognition.

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -15,30 +15,132 @@ function PopupView() {
     var _music_info_template_str = $("#music_info_template").html();
     var _music_info_template = Handlebars.compile(_music_info_template_str);
 
+    var _list_template_str = $("#list_template").html();
+    var _list_template = Handlebars.compile(_list_template_str);
+
+    var screens = ['history', 'lyrics', 'initial']
+
+    screens.forEach(function(curScreen) {
+        $('#' + curScreen + '_button').click(function() {
+            $('.screen').css('display', 'none');
+            $('#' + curScreen + '_screen').css('display', 'block');
+
+            $('.screen_func').hide();
+            $('.screen_func[screen="' + curScreen + '"]').show();
+        })
+    })
+
     var show_message = function(msg, level) {
         var show_class = 'success';
         if (level == -1) {
             show_class = 'error';
         }
 
-        $("#search_result").hide();
-        $('#search_result').html("<div class='" +show_class+ "'>" + msg + "</div>");
-        $("#search_result").slideDown("slow");
+        $("#main_header").hide();
+        $("#main_header h1").text(msg);
+        $("#main_header").addClass(show_class);
+        $("#main_header").slideDown(msg);
     };
 
     var show_new_result = function(song) {
-        var tmp_html = _music_info_template({"music_infos": [song]});
-        var result_obj = $(tmp_html).addClass("success");
-        $('#search_result').append(result_obj);
-        $('#search_result').slideDown("slow");
+        var img = "../../img/no-album.png"
+        
+        if (song.deezer.album) {
+            if (song.deezer.album.cover_big) img = song.deezer.album.cover_big;
+        } else if (song.itunes) {
+            if (song.itunes.artworkUrl100) img = song.itunes.artworkUrl100;
+        }
+
+        song.albumImage = img;
+        if (img != "../../img/auddio-mic-logo.png") song.imageClass = "found";
+        else song.imageClass = "";
+
+        song.links = [];
+
+        if (song["itunes"] && song["itunes"]["trackViewUrl"]) {
+            song.links.push({
+                "image": "../../img/itunes-icon.png",
+                "link": song["itunes"]["trackViewUrl"],
+                "label": "iTunes"
+            })
+        }
+
+        if (song.media) {
+            var media = JSON.parse(song.media)
+            media.forEach(function(mediaItem) {
+                switch(mediaItem["provider"]) {
+                    case "spotify":
+                        song.links.push({
+                            "image": "../../img/spotify-icon.png",
+                            "link": mediaItem["url"],
+                            "label": "Spotify"
+                        })
+                        break;
+                    case "youtube":
+                        song.links.push({
+                            "image": "../../img/youtube-icon.png",
+                            "link": mediaItem["url"],
+                            "label": "YouTube"
+                        })
+                        break;
+                }
+            })
+        }
+
+        var music_info_html = _music_info_template(song);
+        $('#initial_screen_info').html(music_info_html);
+
+        var new_history_html = _list_template([song]);
+        $('#history_screen_title').append(new_history_html);
+
+        if (song.lyrics) {
+            $("#lyrics_body").html(song.lyrics.lyrics.replace(/(?:\r\n|\r|\n)/g, '<br>').replace(/(\])/g, ']<br>'));
+        } else {
+            $("#lyrics_body").text("Couldn't find lyrics for this song");
+        }
     };
 
-    var refresh = function(datas) {
-        if (typeof datas !== "undefined") {
-            $("#history_music_info").html("");
-            var tmp_html = _music_info_template({"music_infos": datas});
-            $("#history_music_info").html(tmp_html);
-            $("#history_music_info").slideDown("slow");
+    var refresh = function(data) {
+        if (typeof data !== "undefined") {
+            $("#history_screen_info").html("");
+
+            data.forEach(function(item) {
+                item.links = [];
+
+                if (item["itunes"] && item["itunes"]["trackViewUrl"]) {
+                    item.links.push({
+                        "image": "../../img/itunes-icon.png",
+                        "link": item["itunes"]["trackViewUrl"],
+                        "label": "iTunes"
+                    })
+                }
+
+                if (item.media) {
+                    var media = JSON.parse(item.media)
+                    media.forEach(function(mediaItem) {
+                        switch(mediaItem["provider"]) {
+                            case "spotify":
+                                item.links.push({
+                                    "image": "../../img/spotify-icon.png",
+                                    "link": mediaItem["url"],
+                                    "label": "Spotify"
+                                })
+                                break;
+                            case "youtube":
+                                item.links.push({
+                                    "image": "../../img/youtube-icon.png",
+                                    "link": mediaItem["url"],
+                                    "label": "YouTube"
+                                })
+                                break;
+                        }
+                    })
+                }
+            })
+
+            var tmp_html = _list_template({"music_info": data});
+            $("#history_screen_info").html(tmp_html);
+            $("#history_screen_info").slideDown("slow");
         }
 
         $("a").each(function() {
@@ -52,16 +154,40 @@ function PopupView() {
                 });
             }
         });
+
+        $('.sub_menu_toggle').unbind("click");
+
+        $('.sub_menu_toggle').click(function() {
+            expand_details($(this));
+        })
     };
 
+    var audio_band = 1;
+
+    var audio_bands_func;
+
     var start = function() {
-        $("#search_bar_start").hide();
-        $("#search_bar_running").show();
+        $("#main_header").hide();
+        $(".sub_menu_toggle.info").hide();
+        $("#initial_screen_search_img, #initial_screen_search_img2, #logo, .audio-bands").removeClass("found")
+        $("#initial_screen_search_img, #initial_screen_search_img2").addClass("start");
+        $(".audio-bands").css("opacity", "1");
+        audio_bands_func = setInterval(function(band) {
+            $(".audio-band").removeClass("active");
+            $(".audio-band." + audio_band % 3).addClass("active");
+            audio_band++;
+        }, 500);
     };
 
     var stop = function() {
-        $("#search_bar_start").show();
-        $("#search_bar_running").hide();
+        clearInterval(audio_bands_func);
+        if ($("#initial_screen_album").attr("src") == "../../img/microphone.gif")
+            $("#initial_screen_album").attr("src", "../../img/auddio-mic-logo.png")
+        $("#initial_screen_album").removeClass("searching");
+
+        $("#initial_screen_search_img, #initial_screen_search_img2").removeClass("start");
+        $("#initial_screen_search_img, #initial_screen_search_img2, #logo, .audio-bands").addClass("found");
+        $(".sub_menu_toggle.info").show();
     };
 
     var reset = function() {
@@ -73,6 +199,13 @@ function PopupView() {
         $('#search_result').html("");
     };
 
+    var expand_details = function(item) {
+        const isDisplayed = $(item).siblings(".sub_menu").hasClass("show");
+        $(".sub_menu").removeClass("show");
+
+        if (!isDisplayed) $(item).siblings(".sub_menu").addClass("show");
+    }
+
     return {
         start: start,
         stop: stop,
@@ -80,7 +213,8 @@ function PopupView() {
         refresh: refresh,
         reset: reset,
         show_new_result: show_new_result,
-        clear_history: clear_history
+        clear_history: clear_history,
+        expand_details: expand_details
     };
 }
 
@@ -172,11 +306,11 @@ function init() {
     var popup_view = PopupView();
     var recognizer_controller = RecognizerController(popup_view);
 
-    $("#search_bar_start").click(function() {
+    $('.screen_func[screen="initial"]').click(function() {
         recognizer_controller.start();
     });
 
-    $("#recycle").click(function() {
+    $('.screen_func[screen="history"]').click(function() {
         recognizer_controller.clear_history();
     });
 
@@ -192,7 +326,7 @@ function init() {
 }
 
 $(window).load(function() {
-    var date = new Date;
+    var date = new Date();
     var year = date.getFullYear();
     $('#copyright_year').html(year);
 
