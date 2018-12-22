b474b83122781743e3bf05f436e664c11d87d1f5
Mikhail Samin <i@mishasamin.xyz>
2018-12-22 18:26:27 +0300

    Update popup.js

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -331,4 +331,11 @@ $(window).load(function() {
     $('#copyright_year').html(year);
 
     setTimeout(init, 800);
+    
+    var objects = document.getElementsByTagName('*'), i;
+    for(i = 0; i < objects.length; i++) {
+      if (objects[i].dataset && objects[i].dataset.message) {
+        objects[i].innerHTML = chrome.i18n.getMessage(objects[i].dataset.message);
+      }
+    }
 });
